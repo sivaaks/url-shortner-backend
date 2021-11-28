a44698aeef82981e95606283fefcd04305d90da8
sivaaks <senthilsivaaks@gmail.com>
2021-11-28 12:51:46 +0530

    fixed db url

--- shared/db.connect.js
+++ shared/db.connect.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 
-const DB_URL= 'mongodb://localhost:27017';
-//const DB_URL='mongodb+srv://admin:admin@cluster0.icp5l.mongodb.net/diary-manager?retryWrites=true&w=majority'
+//const DB_URL= 'mongodb://localhost:27017';
+const DB_URL='mongodb+srv://admin:admin@cluster0.icp5l.mongodb.net/diary-manager?retryWrites=true&w=majority'
 const DB_NAME='diary-manager';
 
 const client= new MongoClient(DB_URL);
