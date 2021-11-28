a065af59399ac8880796fef71f6c20632ad21210
sivaaks <senthilsivaaks@gmail.com>
2021-11-28 12:50:41 +0530

    modified day planner'

--- services/dayPlanner.service.js
+++ services/dayPlanner.service.js
@@ -7,7 +7,7 @@ const dayPlanner= {
     async dayPlan(req,res){
         const {user:{_id}}=getTokenDetails(req.headers.auth);
         try{
-            const data= await db.events.find({date:{$gte:new Date(dbDateNow()),$lt:new Date(dbAddDate(1))},userId:_id},{projection:{userId:0}}).sort({date:1}).toArray();
+            const data= await db.events.find({dateTime:{$gte:new Date(dbDateNow()),$lt:new Date(dbAddDate(1))},userId:_id},{projection:{userId:0}}).sort({dateTime:1}).toArray();
             return res.status(200).send(data);
         }catch(err){
             console.log( `Day planner error ${err}`);
